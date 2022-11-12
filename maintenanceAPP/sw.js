5048dbc29fb91aec6ad8e15ea27fb5ba293f5c37
LucLucero <lucianoxlucero@hotmail.com>
2022-11-12 02:15:04 -0300

    fix: reference links

--- maintenanceAPP/sw.js
+++ maintenanceAPP/sw.js
@@ -4,20 +4,20 @@
 const CACHE_KEY = 'app-main-v1';
 const assetsToCache = [
 
-    "assets/images/book-svgrepo-com.svg",
-    "assets/images/engrenagem.svg",
-    "assets/images/gear.svg",
-    "assets/images/sylvamo-logo-5.png",
-    "assets/images/sylvamo-logo.svg",
-    "assets/scripts/app.js",
-    "assets/scripts/events-query.js",
-    "assets/style/events-page.css",    
-    "assets/style/geareffect.css",
-    "assets/style/home-page-style.css",
-    "events-page/events-page.html",
-    "register-page/register-page.html",
-    "index.html",
-    "sw.js", 
+    "/assets/images/book-svgrepo-com.svg",
+    "/assets/images/engrenagem.svg",
+    "/assets/images/gear.svg",
+    "/assets/images/sylvamo-logo-5.png",
+    "/assets/images/sylvamo-logo.svg",
+    "/assets/scripts/app.js",
+    "/assets/scripts/events-query.js",
+    "/assets/style/events-page.css",    
+    "/assets/style/geareffect.css",
+    "/assets/style/home-page-style.css",
+    "/events-page/events-page.html",
+    "/register-page/register-page.html",
+    "/index.html",
+    "/sw.js", 
     "/",
  
 ];
@@ -29,35 +29,38 @@ async function cacheStaticAssets() {
     
 
 }
-async function networkFirst(request) {   
-        
+
+async function networkFirst(request) {           
         try{
-
             return await fetch (request);
-
         } catch {
-
             console.log("[Service Worker] network error");
-            const cache = await caches.open(CACHE_KEY);
-            return cache.match(request);
-
-
+            return fetchFromCache(request);
         }        
 }
 
-async function cachedFirst(request){
-
-  try {
+// async function cachedFirst(request){
+//   try {
     
 
-  } catch (error) {
+
+//   } catch (error) {
     
 
-  }
+//   }
 
 
 
+// }
+
+async function fetchFromCache(request){
+
+  const cache = await caches.open(CACHE_KEY);
+  const cachedResponse = await cache.match(request);
+  return cachedResponse || null;
 }
+
+
 
 
 self.addEventListener("install", (event) => {
